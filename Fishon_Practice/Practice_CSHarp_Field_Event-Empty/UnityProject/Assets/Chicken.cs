d50f30aeb434f96442fc1e715e3b27365ea3edf2
fishon100 <75553274+fishon100@users.noreply.github.com>
2021-01-02 18:38:15 +0800

    小雞練習

---
+++ Fishon_Practice/Practice_CSHarp_Field_Event-Empty/UnityProject/Assets/Chicken.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("移動速度"), Range(1f, 100f)]
+    public float speed;
+
+    [Header("跳躍高度"), Range(1, 1000)]
+    public int jump;
+
+    [Header("對話內容")]
+    public string talk;
+
+    [Header("是否取得雞蛋")]
+    public bool isegg;
+
+    public void Start()
+    {
+        //使用欄位名稱
+        //取得
+        print("對話:" + talk);
+
+    }
+
+
+
+
+
+
+
+}
