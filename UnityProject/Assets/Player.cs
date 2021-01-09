fcd3c59f15569fd907508fa397ba261336d98fbe
fishon100 <75553274+fishon100@users.noreply.github.com>
2021-01-09 17:57:29 +0800

    赫綵練習20210109
    人物走路 圖片翻轉

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("移動速度"), Range(1, 100)]
+    public float speed = 10.5f;
+
+    public Rigidbody2D Rig;
+
+    public SpriteRenderer Spr;
+
+    //水平值
+    [Header("移動方向"), Range(0, 1)]
+    public float H;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rig = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move();
+        GetHorizontal();
+
+    }
+
+    public void Move()
+    {
+        //Spr.flipX = true;
+
+        Rig.velocity = new Vector2(H * speed, Rig.velocity.y);
+
+
+
+    }
+
+    private void GetHorizontal()
+    {
+        H = Input.GetAxis("Horizontal");
+        if (H == 0)
+        {
+            Spr.flipX = false;
+        }
+        else { Spr.flipX = true; }
+        //取得玩家操控的水平值a/d
+
+
+
+    }
+
+}
